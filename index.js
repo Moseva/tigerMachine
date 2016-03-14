6a7e14f0ef3f50d65f9f521934379a544a741158
Moseva <pywjob@163.com>
2016-03-14 21:17:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,18 +37,18 @@ function go(){
         //停
         clearInterval(timer1);
         var cardTopnow=parseInt($("#cards1").css("top"));
-        var whichcard=Math.floor(cardTopnow/240);
+        var whichcard=Math.floor(cardTopnow/200);
         $("#cards1").css("top",whichcard*20+"rem");
         setTimeout(function(){
             clearInterval(timer2);
             var card2Topnow=parseInt($("#cards2").css("top"));
-            var whichcard2=Math.floor(card2Topnow/240);
+            var whichcard2=Math.floor(card2Topnow/200);
             $("#cards2").css("top",whichcard2*20+"rem");
         },500)
         setTimeout(function(){
             clearInterval(timer3);
             var card3Topnow=parseInt($("#cards3").css("top"));
-            var whichcard3=Math.floor(card3Topnow/240);
+            var whichcard3=Math.floor(card3Topnow/200);
             $("#cards3").css("top",whichcard3*20+"rem");
             recoverBtn();
         },1000)
